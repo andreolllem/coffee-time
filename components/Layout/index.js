9a79f1c18b66fea54042a33206a5a9a0799bef4a
Andre Mello <andremello@MacBook-Pro-de-Andre.local>
2021-01-13 07:56:49 -0300

    Error Layout module

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import styles from "../../styles/layout.module.css";
+import styles from "../../styles/Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
